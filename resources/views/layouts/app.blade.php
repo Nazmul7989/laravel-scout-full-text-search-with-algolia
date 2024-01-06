1f0d74f4fdf441950d7d9543224418c542c52391
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2024-01-06 19:10:10 +0600

    search with instant search js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+
+    <script src="https://cdn.jsdelivr.net/npm/algoliasearch@4.22.0/dist/algoliasearch-lite.umd.js" integrity="sha256-/2SlAlnMUV7xVQfSkUQTMUG3m2LPXAzbS8I+xybYKwA=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.63.0/dist/instantsearch.production.min.js" integrity="sha256-tP2geWC/2cT8rjeVSRumDQXTieBNjUyZjvON1HzWqso=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@8.1.0/themes/satellite-min.css" integrity="sha256-p/rGN4RGy6EDumyxF9t7LKxWGg6/MZfGhJM/asKkqvA=" crossorigin="anonymous">
+
+
+
 </head>
 <body>
 
@@ -26,5 +33,49 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
 <script src="{{ asset('js/scripts.js') }}"></script>
+
+<script>
+    const searchClient = algoliasearch('{{ config('scout.algolia.id') }}', '{{ config('scout.algolia.secret') }}');
+
+    const search = instantsearch({
+        indexName: 'products_index',
+        searchClient,
+    });
+
+    search.addWidgets([
+        instantsearch.widgets.searchBox({
+            container: '#searchbox',
+        }),
+
+        instantsearch.widgets.hits({
+            container: '#hits',
+            templates: {
+                item(hit, { html, components }) {
+                    return html`
+                    <h2>
+                      ${hit.__hitIndex}:
+                      ${components.Highlight({ attribute: 'title', hit })}
+                    </h2>
+                    <p>${hit.description}</p>
+                  `;
+                },
+            },
+
+        }),
+
+        instantsearch.widgets.configure({
+            hitsPerPage: 5,
+        }),
+        instantsearch.widgets.pagination({
+            container: '#pagination',
+        }),
+
+    ]);
+
+    search.start();
+
+</script>
+
+
 </body>
 </html>
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -8,10 +8,9 @@
                 <li class="nav-item"><a class="nav-link {{ request()->is('/') ? 'active' : '' }}" aria-current="page" href="{{ route('home') }}">Home</a></li>
                 <li class="nav-item"><a class="nav-link {{ request()->is('/products') ? 'active' : '' }}" href="{{ route('products.index') }}">Products</a></li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+
+            <div id="searchbox"></div>
         </div>
     </div>
 </nav>
+
