24c21926c58bb45883ac0e313f5f76b371ef637d
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2024-01-26 09:44:28 +0600

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -20,7 +20,7 @@
                     <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="{{ $product->image }}" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
